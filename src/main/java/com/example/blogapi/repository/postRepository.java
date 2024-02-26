a4d4bd45200a5bbccd2dd0910cb2d4dae25d9164
priyesh <priyeshsomiya@gmail.com>
2024-02-26 09:15:25 +0530

    Revert "first"
    This reverts commit 2e211e0cb4e4ea57fd3f1cca6c7ddd58cba52278.

--- src/main/java/com/example/blogapi/repository/postRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.blogapi.repository;
-
-import com.example.blogapi.entity.post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface postRepository extends JpaRepository<post,Long> {
-}
--- src/main/java/com/example/blogapi/service/postService.java
+++
@@ -1,7 +0,0 @@
-package com.example.blogapi.service;
-
-import com.example.blogapi.payload.postDto;
-
-public interface postService {
-    public postDto createpo(postDto postdto);
-}
